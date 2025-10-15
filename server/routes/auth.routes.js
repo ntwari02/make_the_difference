e055695a02629364a991120ddb3c79c0faf6dfe3
ntwari02 <166018104+ntwari02@users.noreply.github.com>
2025-10-15 05:49:51 +0200

    Removed recaptcha on Auth forms error solved

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,13 +2,12 @@ const { Router } = require('express');
 const AuthController = require('../controllers/auth.controller');
 const OauthController = require('../controllers/oauth.controller');
 const { authenticate, authorizeRoles } = require('../middlewares/auth');
-const { verifyRecaptcha } = require('../middlewares/recaptcha.middleware');
 
 const router = Router();
 
 // Public routes
-router.post('/register', AuthController.register); // reCAPTCHA temporarily disabled
-router.post('/login', verifyRecaptcha, AuthController.login);
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
 router.post('/refresh', AuthController.refresh);
 router.post('/logout', AuthController.logout);
 router.post('/magic/request', AuthController.requestMagicLink);
